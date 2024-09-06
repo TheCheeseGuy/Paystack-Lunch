4aa467b90f6e06ed83382480b6d77519cb9b80fa
The Cheese Guy <jappiah-aberde@lincoln.edu.gh>
2024-09-06 11:17:22 +0000

    server change

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const errorHandler = require('middleware/error-handler')
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(cookieParser())
-app.set('view engine', 'ejs')
 
 app.use(cors({ origin: (origin, callback) => callback(null, true), credentials: true }))
 
@@ -18,5 +17,5 @@ app.use('/lcs', require('./controller/transactionController'));
 app.enable('trust-proxy')
 app.use(errorHandler)
 
-const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080 ) : 3001;
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 8080 ) : 3000;
 app.listen(port, () => console.log('Server listening on port ' + port ))
